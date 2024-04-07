263d13fd9fa62dd64474a6ecf16f7a013340de36
Yuexing Li <lyxidontknow@gmail.com>
2024-04-07 05:58:01 -0400

    Create cityscapes_dataset.py

---
+++ model/cityscapes_dataset.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class CityscapesDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(self.image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index].replace('_color.png', '_instanceIds.png'))
+
+        image = np.array(Image.open(image_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"))
+
+        if self.transform is not None:
+            augmentation = self.transform(image=image, mask=mask)
+            image = augmentation["image"]
+            mask = augmentation["mask"]
+
+        return image, mask
