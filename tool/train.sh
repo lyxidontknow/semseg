5a01497ac74be00a6420966a97d8656c63fe6dae
Yuexing Li <lyxidontknow@gmail.com>
2024-04-07 05:58:18 -0400

    Update train.sh

--- tool/train.sh
+++ tool/train.sh
@@ -18,14 +18,9 @@ result_dir=${exp_dir}/result
 config=config/${dataset}/${dataset}_${exp_name}.yaml
 now=$(date +"%Y%m%d_%H%M%S")
 
-mkdir -p ${model_dir} ${result_dir}
-cp tool/train.sh tool/train.py tool/test.sh tool/test.py ${config} ${exp_dir}
+mkdir -p "${model_dir}" "${result_dir}"
+cp tool/train.sh tool/train.py tool/test.sh tool/test.py "${config}" "${exp_dir}"
 
 export PYTHONPATH=./
-$PYTHON -u ${exp_dir}/train.py \
-  --config=${config} \
-  2>&1 | tee ${model_dir}/train-$now.log
-
-$PYTHON -u ${exp_dir}/test.py \
-  --config=${config} \
-  2>&1 | tee ${result_dir}/test-$now.log
+$PYTHON -u "${exp_dir}"/train.py --config="${config}" 2>&1 | tee "${model_dir}"/train-"${now}".log
+$PYTHON -u "${exp_dir}"/test.py --config="${config}" 2>&1 | tee "${result_dir}"/test-"${now}".log
